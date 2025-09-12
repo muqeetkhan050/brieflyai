6878992871ba6808fa64d13e640f3e356df9dcad
muqeetkhan <muqeetkhan050@gmail.com>
2025-09-13 02:13:35 +1000

    added header

---
+++ src/components/Mainpage.js
@@ -0,0 +1,16 @@
+import React,{ useState } from 'react';
+import Header from './Header'
+
+const Mianpage=()=>{
+    return(
+        <div>
+
+
+            <h1>here we will have our github profile and BrieflyAI</h1>
+            <Header/>
+
+        </div>
+    )
+}
+
+export default Mianpage;
